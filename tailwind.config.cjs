a0f2d06336c7d2fcfc6dd32143a39a6a77a0e01b
moonayyur <jm_kias@esi.dz>
2022-12-03 04:19:33 +0100

    Fixes

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -6,6 +6,11 @@ module.exports = {
       fontFamily: {
         poppins: ["poppins", "sans-serif"],
       },
+      colors: {
+        blue: "#5E5B70",
+        clear: "#FEFEFE",
+        candy: "#ECE0E5",
+      },
     },
   },
   plugins: [],
